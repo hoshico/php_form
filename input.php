c734a74029976dc99050ee4b7a673192d204f53d
hoshico <melancholinista.yasu@gmail.com>
2021-06-22 07:58:33 +0900

    フォーム実装

--- input.php
+++ input.php
@@ -1,11 +1,21 @@
 <?php
 
-if(!empty($_GET)){
+if(!empty($_POST)){
   echo '<pre>';
-  var_dump($_GET);
+  var_dump($_POST);
   echo '</pre>';
 // スーパーグローバル変数
 }
+
+$pageFlag =0;
+
+if(!empty($_POST['btn-confirm'])){
+  $pageFlag = 1;
+}
+if(!empty($_POST['btn-submit'])){
+  $pageFlag = 2;
+}
+
 ?>
 
 
@@ -14,19 +24,44 @@ if(!empty($_GET)){
 <meta charset="UTF-8">
 <head></head>
 <body>
-  
-<form method="GET" action='input.php'>
+
+
+
+<?php if($pageFlag === 1) : ?>
+<form method="POST" action='input.php'>
 氏名
-<input type="text" name="your_name">
+<?php echo $_POST['your_name'] ;?>
 <br>
-<input type="checkbox" name="sports[]" value="野球">野球
-<input type="checkbox" name="sports[]" value="サッカー">サッカー
-<input type="checkbox" name="sports[]" value="バスケ">バスケ
-<input type="submit" value="送信">
+メールアドレス
+<?php echo $_POST['email'] ;?>
+<br>
+<input type="submit" name="back" value="戻る">
+<input type="submit" name="btn-submit" value="送信する">
+<input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
+<input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
+</form>
+<?php endif; ?>
+
+<?php if($pageFlag === 2) : ?>
+送信が完了しました
+<?php endif; ?>
 
 
+
+
+<?php if($pageFlag === 0) : ?>
+
+<form method="POST" action='input.php'>
+氏名
+<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];}?>">
+<br>
+メールアドレス
+<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'];}?>">
+<br>
+<input type="submit" name="btn-confirm" value="確認する">
 
 </form>
+<?php endif; ?>
 
 </body>
 </html>
