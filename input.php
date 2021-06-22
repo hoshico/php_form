791bec787306821cc9ba36301492e0313418add0
hoshico <melancholinista.yasu@gmail.com>
2021-06-22 21:32:32 +0900

    bootstrap

--- input.php
+++ input.php
@@ -3,21 +3,25 @@
 
 session_start();
 
+require 'validation.php';
+
 header("X-FRAME-OPTIONS: DENY");
 
-if(!empty($_SESSION)){
+if(!empty($_POST)){
   echo '<pre>';
-  var_dump($_SESSION);
+  var_dump($_POST);
   echo '</pre>';
 // スーパーグローバル変数
 }
 
 $pageFlag =0;
+$errors = validation($_POST);
 
-if(!empty($_POST['btn-confirm'])){
+
+if(!empty($_POST['btn_confirm'] && empty($errors))){
   $pageFlag = 1;
 }
-if(!empty($_POST['btn-submit'])){
+if(!empty($_POST['btn_submit'])){
   $pageFlag = 2;
 }
 
@@ -30,9 +34,18 @@ function h($str)
 
 
 
-<!DOCTYPE html>
-<meta charset="UTF-8">
-<head></head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>私は仕事が大好きです。</title>
+  </head>
 <body>
 
 
@@ -46,10 +59,37 @@ function h($str)
 メールアドレス
 <?php echo h($_POST['email']) ;?>
 <br>
+ホームページ
+<?php echo h($_POST['url']) ;?>
+<br>
+性別
+<?php
+  if($_POST['gender'] === '0'){ echo '男性';}
+  if($_POST['gender'] === '1'){ echo '女性';}
+?>
+<br>
+年齢
+<?php
+  if($_POST['age'] === '1'){ echo '〜19歳' ;}
+  if($_POST['age'] === '2'){ echo '20歳〜29歳' ;}
+  if($_POST['age'] === '3'){ echo '30歳〜39歳' ;}
+  if($_POST['age'] === '4'){ echo '40歳〜49歳' ;}
+  if($_POST['age'] === '5'){ echo '50歳〜59歳' ;}
+  if($_POST['age'] === '6'){ echo '60歳〜69歳' ;}
+?>
+<br>
+お問い合わせ内容
+<?php echo h($_POST['contact']) ;?>
+<br>
+
 <input type="submit" name="back" value="戻る">
-<input type="submit" name="btn-submit" value="送信する">
+<input type="submit" name="btn_submit" value="送信する">
 <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
 <input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
+<input type="hidden" name="url" value="<?php echo h($_POST['url']) ;?>">
+<input type="hidden" name="gender" value="<?php echo h($_POST['gender']) ;?>">
+<input type="hidden" name="age" value="<?php echo h($_POST['age']) ;?>">
+<input type="hidden" name="contact" value="<?php echo h($_POST['contact']) ;?>">
 <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ;?>">
 </form>
 
@@ -77,17 +117,87 @@ if(!isset($_SESSION['csrfToken'])){
 $token = $_SESSION['csrfToken'];
 
 ?>
-<form method="POST" action='input.php'>
-氏名
-<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);}?>">
-<br>
-メールアドレス
-<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);}?>">
-<br>
-<input type="submit" name="btn-confirm" value="確認する">
-<input type="hidden" name="csrf" value="<?php echo $token; ?>">
-</form>
+
+<?php if(!empty($errors) && !empty($_POST['btn_confirm'])) :?>
+<?php echo '<ul>' ;?>
+<?php
+  foreach($errors as $error){
+    echo '<li>' . $error . '</li>' ;
+  }
+?>
+<?php echo '</ul>' ;?>
+
+
+<?php endif ; ?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+    <form method="POST" action='input.php'>
+    <div class="form-group">
+      <label for="your_name">氏名</label>
+      <input type="text" class="form-control" id="your_name" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);}?>" required>
+    </div>
+    
+    <div class="form-group">
+      <label for="email">メールアドレス</label>
+      <input type="email" class="form-control" id="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);}?>" required>
+    </div>  
+
+    <div class="form-group">
+      <label for="url">ホームページ</label>
+      <input type="url" class="form-control" id="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']);}?>">
+    </div>
+性別
+    <div class="form-check form-check-inline">
+      <input type="radio" class="form-check-input" name="gender" id="gender1" value="0" 
+      <?php if(!empty($_POST['gender']) && $_POST['gender'] === '0')
+      { echo 'checked';} ?>>
+      <label class="form-check-label" for="gender1">男性</label>
+      <input type="radio" class="form-check-input" name="gender" id="gender1" value="1"
+      <?php if(!empty($_POST['gender']) && $_POST['gender'] === '1')
+      { echo 'checked';} ?>>
+      <label class="form-check-label" for="gender2">女性</label>
+    </div>
+  
+    <div class="form-group">
+    <label for="age">年齢</label>
+    <select class="form-control" id="name" name="age">
+      <option value="">選択してください</option>
+      <option value="1" selected>〜19歳</option>
+      <option value="2">20歳〜29歳</option>
+      <option value="3">30歳〜39歳</option>
+      <option value="4">40歳〜49歳</option>
+      <option value="5">50歳〜59歳</option>
+      <option value="6">60歳〜69歳</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+    <label for="contact">お問い合わせ内容</label>
+    <textarea class="form-control" id="contact" row="3" row="3" name="contact">
+    <?php if(!empty($_POST['contact'])){echo h($_POST['contact']);}?>
+    </textarea>
+    </div>
+
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="caution" name="caution" value="1">
+      <label class="form-check-input" for="caution">注意事項のチェック</label>
+    </div>
+
+    <input class="btn- btn-info" type="submit" name="btn_confirm" value="確認する">
+    <input type="hidden" name="csrf" value="<?php echo $token; ?>">
+    </form>
+    
+    </div>
+  </div>
+</div>
+
 <?php endif; ?>
 
+<!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
